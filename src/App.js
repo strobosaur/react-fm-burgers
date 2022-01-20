e191d5c42bca887c27e1f1a016f0a150b4434970
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-01-20 13:32:12 +0100

    basic CSS, basic app.js functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState } from 'react'
+
+// CSS STYLES
 import './App.css';
 
 function App() {
+  const [burger, setBurger] = useState({ bread: "", toppings: [] });
+
+  const addBread = (bread) => {
+    setBurger({...burger, bread})
+  }
+
+  const addTopping = (topping) => {
+    let newToppings;
+    if(!burger.toppings.includes(topping)){
+      newToppings = [...burger.toppings, topping];
+    } else {
+      newToppings = burger.toppings.filter(item => item !== topping);
+    }
+    setBurger({ ...burger, toppings: newToppings });
+  }
+
   return (
     <div className="App">
       
