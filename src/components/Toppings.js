99e1c22df17947ee4095b81659255d8349875746
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-01-23 17:57:00 +0100

    animation states using variants variables

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion'
 
 const Toppings = ({ addTopping, burger }) => {
   let toppings = ['pickled onion', 'pickled cucumber', 'onion', 'tomato', 'salad', 'Sriracha Mayo', 'Classic dressing', 'BBQ Sauce', 'Ketchup', 'Truffel mayo']
@@ -12,15 +13,26 @@ const Toppings = ({ addTopping, burger }) => {
         {toppings.map(topping => {
           let spanClass = burger.toppings.includes(topping) ? 'active' : '';
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
+            <motion.li key={topping} onClick={() => addTopping(topping)}
+              whileHover={{ scale: 1.1, originX:0, color: '#f8e112'}}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
               <span className={spanClass}>{ topping }</span>
-            </li>
+            </motion.li>
           )
         })}
       </ul>
 
       <Link to="/order">
-        <button>Order</button>
+        <motion.button
+          whileHover={{ 
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 8px rgb(255,255,255)",
+          }}   
+        >
+          Order
+        </motion.button>
       </Link>
 
     </div>
