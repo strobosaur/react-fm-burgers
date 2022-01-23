420e6e784bd93dca4fb82a16b20e25b99917ab51
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-01-23 14:20:27 +0100

    Order Component prototype

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Order = () => {
+const Order = ({ burger }) => {
   return (  
-    <div className="base-container">
-      
+    <div className="order-container">
+      <h2>Thank you for your order!</h2>
+      <p>You ordered a {burger.bread} burger with:</p>
+      {burger.toppings.map(topping => <div key={topping}>{topping}</div>)}
     </div>
   );
 }
